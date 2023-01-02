3fba382c3d904fe605204bb803b38d02343e6224
Ali Subhani <ali.subhani@tintash.com>
2023-01-02 12:44:19 +0500

    Create typescript project, add required components and remvoe unused code

---
+++ to-do-list/src/App.tsx
@@ -0,0 +1,20 @@
+import React, { FC } from "react";
+import "./App.css";
+
+const App: FC = () => {
+  return (
+    <div className="App">
+      <div className="header">
+        <div className="input-container">
+          <input type="text" placeholder="Task..." />
+          <input type="text" placeholder="Deadline (in Days)..." />
+        </div>
+
+        <button>Add Task</button>
+      </div>
+      <div className="todoList"></div>
+    </div>
+  );
+};
+
+export default App;
